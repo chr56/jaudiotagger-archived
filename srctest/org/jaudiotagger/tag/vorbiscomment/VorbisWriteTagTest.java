b5dc5c0cf10dac9e79fce6e73f607adaa7b6d957
Mykola Makhin <mvmn.inbox@gmail.com>
2021-10-02 13:48:23 +0300

    Unit test fix - reset WriteMp3GenresAsText option to false in VorbisWriteTagTest.testWriteTagToFile

--- srctest/org/jaudiotagger/tag/vorbiscomment/VorbisWriteTagTest.java
+++ srctest/org/jaudiotagger/tag/vorbiscomment/VorbisWriteTagTest.java
@@ -51,6 +51,7 @@ public class VorbisWriteTagTest extends AbstractTestCase
      */
     public void testWriteTagToFile()
     {
+    	TagOptionSingleton.getInstance().setWriteMp3GenresAsText(false);
         TagOptionSingleton.getInstance().setVorbisAlbumArtistReadOptions(VorbisAlbumArtistReadOptions.READ_ALBUMARTIST);
         Exception exceptionCaught = null;
         try
